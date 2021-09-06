b581b1c55fa3b22459fcb9612195610facd8dd27
jsnoap <jordan.snoap@gmail.com>
2021-09-06 13:04:39 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    // Program to return what year and how many years until you can retire based off of user input.
+    public static void main( String[] args )
+    {
+        int age, retireAge;
+
+        Date d = new Date();
+        int year = d.getYear();
+        year = year + 1900;
+
+        System.out.println("What is your current age?");
+        Scanner inputAge = new Scanner(System.in);
+        age = inputAge.nextInt();
+
+        System.out.println("At what age would you like to retire?");
+        Scanner inputRetire = new Scanner(System.in);
+        retireAge = inputRetire.nextInt();
+
+        System.out.println("You have " + (retireAge - age) + " years left until you can retire.");
+        System.out.println("It's " + year + ", so you can retire in " + (year + (retireAge - age)) + "." );
+    }
+}
